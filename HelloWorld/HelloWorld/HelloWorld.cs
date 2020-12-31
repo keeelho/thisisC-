b45030c69a84fb0c11d7c3dd4ac183a1dd22aa29
unknown <keeelho@gmail.com>
2020-12-31 17:39:59 +0900

    first commit on thisiscsharp

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thisisCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("사용법 : HelloWorld.exe <이름>");
+                return;
+            }
+            Console.WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
